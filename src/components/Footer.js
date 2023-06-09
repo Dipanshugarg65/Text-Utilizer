98ae4f2a4263b19cf7517e4c51c083b2bc899090
Dipanshugarg <dipanshugarg65@gmail.com>
2023-06-09 22:04:19 +0530

    Change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,4 @@
-// import React from 'react'
 
-// const Footer = () => {
-//   return (
-//     <div>
-//       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top ">
-//         <div className="col-md-4 d-flex align-items-center">
-//           <span className="text-muted">
-//             <h3 align="center"> Copyright © 2023 Text-Utilizer</h3>
-//           </span>
-//         </div>
-//       </footer>
-//     </div>
-//   );
-// }
-
-// export default Footer
 import React from "react";
 
 export default function Footer() {
